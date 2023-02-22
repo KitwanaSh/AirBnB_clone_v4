fec8513c5fc886eb83647a0fd4ef78d8ec8fc866
KitwanaSh <rodriguekitweze@gmail.com>
2023-02-22 20:29:29 +0200

    Feat: add the API status

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,7 +8,8 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
