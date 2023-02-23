81d093bf313a5edee956b886237e7b07c7772410
KitwanaSh <rodriguekitweze@gmail.com>
2023-02-23 14:48:34 +0200

    Fix: Update the task 3

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -8,40 +8,31 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
 
+@app.route('/3-hbnb')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
+    return render_template('3-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
     storage.close()
 
 
-@app.route('/3-hbnb/', strict_slashes=False)
-def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    return render_template('0-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places,
-                           cache_id=uuid.uuid4())
-
-
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
